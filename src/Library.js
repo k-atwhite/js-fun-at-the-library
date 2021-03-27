d6055daed183dccd4aa6a61c2a2db8ec1f30c328
Kat White <kat.white.a@gmail.com>
2021-03-27 13:56:11 -0600

    Complete Library implementation

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,33 @@
+function createLibrary(libraryName) {
+  var library = {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+  return library
+}
+
+function addBook(library, bookTitle) {
+ library.shelves[bookTitle.genre].push(bookTitle)
+}
+
+function checkoutBook(library, title, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    var book = library.shelves[genre][i]
+    if (book.title === title) {
+      library.shelves[genre].splice(i,1)
+      return  `You have now checked out ${title} from the ${library.name}`
+    }
+  }
+  return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+}
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
